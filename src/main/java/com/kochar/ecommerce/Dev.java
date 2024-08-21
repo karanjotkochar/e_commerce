24aa87080bde0929392d6c58a6ff869d3d9e8e9f
karanjotkochar <karanjot.kochar@gmail.com>
2024-08-21 01:33:34 -0400

    add Laptop class

--- src/main/java/com/kochar/ecommerce/Dev.java
+++ src/main/java/com/kochar/ecommerce/Dev.java
@@ -1,11 +1,17 @@
 package com.kochar.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
+    @Autowired // Field injection
+    private Laptop laptop;
+
     public void build() {
+
+        laptop.compile();
         System.out.println("Begin project");
     }
 }
