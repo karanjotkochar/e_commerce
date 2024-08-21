91f03df2e0ac4d839edec31461d2a7772dbb0712
karanjotkochar <karanjot.kochar@gmail.com>
2024-08-21 01:39:33 -0400

    other injection methods

--- src/main/java/com/kochar/ecommerce/Dev.java
+++ src/main/java/com/kochar/ecommerce/Dev.java
@@ -9,6 +9,17 @@ public class Dev {
     @Autowired // Field injection
     private Laptop laptop;
 
+    // Constructor injection
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+    // Setter injection
+//    @Autowired
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
     public void build() {
 
         laptop.compile();
